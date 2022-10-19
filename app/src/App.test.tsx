9c022f5e7036e4c0e3d0fe0b1c506039193f7f33
Charles Forson <59391676+ShiftySlothe@users.noreply.github.com>
2022-10-19 12:44:11 +0100

    Bootstraps CRA with Chakra

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from '@testing-library/react'
-import React from 'react'
-import App from './App'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test('renders hello world', () => {
-  render(<App />)
-  const spanElement = screen.getByText(/Hello world/i)
-  expect(spanElement).toBeInTheDocument()
-})
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,75 +1,26 @@
-import { LoadingButton } from '@mui/lab'
-import { Box, CircularProgress, Paper, Stack, TextField } from '@mui/material'
-import CssBaseline from '@mui/material/CssBaseline'
-import Grid from '@mui/material/Grid'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import React from 'react'
-import './App.css'
-
-const theme = createTheme()
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const onSubmit = () => {}
   return (
-    <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
-        <CssBaseline />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <span>Hello world</span>
-
-            <Box component="form" noValidate onSubmit={onSubmit} sx={{ mt: 1 }}>
-              <Stack gap={2} width={'100%'}>
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="title"
-                  label="Title"
-                  name="title"
-                  autoFocus
-                />
-                <LoadingButton 
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                >
-                  Build
-                </LoadingButton>
-              </Stack>
-            </Box>
-          </Box>
-        </Grid>
-
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          <Box
-            sx={{
-              my: 8,
-              mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <CircularProgress />
-          </Box>
-        </Grid>
-      </Grid>
-    </ThemeProvider>
-  )
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
