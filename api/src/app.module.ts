420b9e4dd2fa8667db44362a12f5147a212e85e6
Charles Forson <59391676+ShiftySlothe@users.noreply.github.com>
2022-10-19 11:55:01 +0100

    Generates job post controller

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JobPostModule } from './job-post/job-post.module';
 
 @Module({
-  imports: [],
+  imports: [JobPostModule],
   controllers: [AppController],
   providers: [AppService],
 })
