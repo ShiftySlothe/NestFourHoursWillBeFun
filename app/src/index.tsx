e37b768dce4675c5d4eb45edef3f4bf2f0436b3a
Charles Forson <59391676+ShiftySlothe@users.noreply.github.com>
2022-10-19 14:02:11 +0100

    Removes unused import

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,6 +2,7 @@ import { ChakraProvider, theme } from "@chakra-ui/react";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { UserType } from "./types";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
