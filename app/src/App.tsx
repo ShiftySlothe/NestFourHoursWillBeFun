75801320874895b0264b699fe12a51294f1bff56
Charles Forson <59391676+ShiftySlothe@users.noreply.github.com>
2022-10-19 13:11:55 +0100

    Add header

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,31 @@
 import * as React from "react";
-import { ChakraProvider, theme } from "@chakra-ui/react";
-import Page from "./Page";
+import {
+  Container,
+  Flex,
+  Heading,
+  FormControl,
+  Select,
+  Box,
+} from "@chakra-ui/react";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Page />
-  </ChakraProvider>
-);
+export const App = () => {
+  return (
+    <Container maxW="1600px" paddingX={{ base: "5px", md: "40px", lg: "60px" }}>
+      <Flex
+        alignItems="center"
+        justifyContent="space-between"
+        paddingY={{ base: "5px", md: "20px", lg: "30px" }}
+      >
+        <Heading>Job Board</Heading>
+        <Box>
+          <FormControl display="flex" alignItems="center">
+            <Select placeholder="Select role">
+              <option value="solicitor">Solicitor</option>
+              <option value="client">Client</option>
+            </Select>
+          </FormControl>
+        </Box>
+      </Flex>
+    </Container>
+  );
+};
