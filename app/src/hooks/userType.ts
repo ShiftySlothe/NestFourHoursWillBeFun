cd445a06def34a87b2ad33f6b9a33c4e924836f6
Charles Forson <59391676+ShiftySlothe@users.noreply.github.com>
2022-10-19 14:02:25 +0100

    Creates user hooks

---
+++ app/src/hooks/userType.ts
@@ -0,0 +1,24 @@
+import React from "react";
+import { UserType } from "../types";
+
+export function useUserType() {
+  const [userType, setUserType] = React.useState<UserType | null>(null);
+  const value = { user: userType, setUserType };
+
+  return { value, setUserType };
+}
+
+export function useGetUserTypeFromLocalStorage() {
+  const { setUserType, value } = useUserType();
+  React.useEffect(() => {
+    // Check local storage for userType
+    const userType = localStorage.getItem("userType");
+    if (userType) {
+      console.log("User type found");
+      console.log(userType);
+      setUserType(userType as UserType);
+    }
+  }, [setUserType]);
+
+  return value;
+}
