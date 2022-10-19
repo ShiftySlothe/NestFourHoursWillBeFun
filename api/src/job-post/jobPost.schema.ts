39a52bb0f3fcc06b24bf07858cd03dc9368e1dfe
Charles Forson <59391676+ShiftySlothe@users.noreply.github.com>
2022-10-19 16:06:25 +0100

    Updates schema with no win no fee

--- api/src/job-post/jobPost.schema.ts
+++ api/src/job-post/jobPost.schema.ts
@@ -4,10 +4,12 @@ export interface JobPost {
   title: string;
   description: string;
   started: boolean;
+  feeStructure: 'noWinNoFee' | 'fixedFee';
 }
 
 export const JobPostSchema = new mongoose.Schema<JobPost>({
   title: { type: String, required: true },
   description: { type: String, required: true },
   started: { type: Boolean, required: true },
+  feeStructure: { type: String, required: true },
 });
