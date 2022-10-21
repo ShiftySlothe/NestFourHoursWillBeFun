ad465187a8e0c627e6a2677890d0d14f1261cfb3
Charles Forson <59391676+ShiftySlothe@users.noreply.github.com>
2022-10-21 10:09:10 +0100

    Add job modal

--- app/src/sections/AllJobs.tsx
+++ app/src/sections/AllJobs.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
-import { Box, Container, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Heading,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { JobPost } from "../types";
 
 export default function AllJobs() {
@@ -21,20 +33,56 @@ export default function AllJobs() {
     <Container>
       <Heading>All Jobs</Heading>
       {query.data &&
-        query.data.data.map((job: JobPost) => (
-          <Box
-            key={job.id}
-            border="1px solid black"
-            borderRadius="5px"
-            padding={2}
-            marginY={4}
-            boxShadow="md"
-          >
-            <Heading size="md">{job.title}</Heading>
-            <Text>{job.description}</Text>
-            <Text>{job.feeStructure}</Text>
-          </Box>
-        ))}
+        query.data.data.map((job: JobPost) => <JobPostItem job={job} />)}
     </Container>
+  );
+}
+
+function JobPostItem({ job }: { job: JobPost }) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Box
+        onClick={onOpen}
+        key={job.id}
+        border="1px solid black"
+        borderRadius="5px"
+        padding={2}
+        marginY={4}
+        boxShadow="md"
+        cursor={"pointer"}
+        _hover={{ boxShadow: "lg", transform: "scale(1.001)" }}
+        transition="all 0.2s"
+      >
+        <Heading size="md">{job.title}</Heading>
+        <Text>{job.description}</Text>
+        <Text>{job.feeStructure}</Text>
+      </Box>
+      <JobPostModal job={job} isOpen={isOpen} onClose={onClose} />
+    </>
+  );
+}
+
+function JobPostModal({
+  job,
+  isOpen,
+  onClose,
+}: {
+  job: JobPost;
+  isOpen: boolean;
+  onClose: () => void;
+}) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>{job.title}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text>{job.description}</Text>
+          <Text>{job.feeStructure}</Text>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
   );
 }
