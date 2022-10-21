56d5aff1c1ba7a21de7e7dd2efff4ecf9625b940
Charles Forson <59391676+ShiftySlothe@users.noreply.github.com>
2022-10-21 10:24:00 +0100

    Adds queries to job post

--- app/src/sections/AllJobs.tsx
+++ app/src/sections/AllJobs.tsx
@@ -3,7 +3,9 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import {
   Box,
+  Button,
   Container,
+  Flex,
   Heading,
   Modal,
   ModalBody,
@@ -15,7 +17,8 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 import { JobPost } from "../types";
-
+import { useMutation } from "react-query";
+import { queryClient } from "../App";
 export default function AllJobs() {
   const query = useQuery("jobs", () =>
     axios.get("http://localhost:4000/job-post")
@@ -45,14 +48,14 @@ function JobPostItem({ job }: { job: JobPost }) {
       <Box
         onClick={onOpen}
         key={job.id}
-        border="1px solid black"
-        borderRadius="5px"
+        border={"1px solid black"}
+        borderRadius={"5px"}
         padding={2}
         marginY={4}
         boxShadow="md"
         cursor={"pointer"}
         _hover={{ boxShadow: "lg", transform: "scale(1.001)" }}
-        transition="all 0.2s"
+        transition={"all 0.2s"}
       >
         <Heading size="md">{job.title}</Heading>
         <Text>{job.description}</Text>
@@ -72,6 +75,24 @@ function JobPostModal({
   isOpen: boolean;
   onClose: () => void;
 }) {
+  const deleteMutation = useMutation(
+    () => axios.delete(`http://localhost:4000/job-post/${job.id}`),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("jobs");
+      },
+    }
+  );
+
+  const setPaidMutation = useMutation(
+    () => axios.put(`http://localhost:4000/job-post/${job.id}`, { paid: true }),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("jobs");
+      },
+    }
+  );
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
@@ -81,6 +102,20 @@ function JobPostModal({
         <ModalBody>
           <Text>{job.description}</Text>
           <Text>{job.feeStructure}</Text>
+          <Flex
+            direction={"row"}
+            gap={3}
+            alignItems={"center"}
+            justifyContent={"space-between"}
+            mt={3}
+            width={"100%"}
+          >
+            <Text>Fee total: {job.paidAmount}</Text>
+            <Button disabled={job.paid}>
+              {job.paid ? "Job paid" : "Mark paid"}
+            </Button>
+          </Flex>
+          <Button>Delete</Button>
         </ModalBody>
       </ModalContent>
     </Modal>
